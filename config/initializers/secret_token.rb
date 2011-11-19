b069853e66506539d7dbbbe4713d9b915d7f3fe0
isac coelho <isac.coelho@gmail.com>
2011-11-18 22:49:20 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codedojo::Application.config.secret_token = '44c8c43c3c09066ff1b8c8d95b798e8a0562be3f4ae6512df9ebd027f537c90022eb7b4ef14af51a788335995b7cf9bfe7af48fb18c640151faf818e3ff3cc98'
